77331abb0dc31fd131fe6ef36fb662b539333855
MuddyGames <make@muddygames.com>
2026-02-09 22:04:32 +0000

    QEMU HEadless

--- rules/qemu.mk
+++ rules/qemu.mk
@@ -12,7 +12,7 @@ __qemu_test: __validate_r36s_platform $(TARGET)
 	$(call INFO_MSG_HEAD,$(MSG_QEMU_START))
 	$(call INFO_MSG,Running $(TARGET) in QEMU)
 	@cp -r "$(ASSETS_DIR)" "$(BUILD_DIR)/"
-	@qemu-aarch64 -L /usr/aarch64-linux-gnu "./$(TARGET)" >qemu_test.log 2>&1; \
+	@QEMU_HEADLESS=1 qemu-aarch64 -L /usr/aarch64-linux-gnu "./$(TARGET)" >qemu_test.log 2>&1; \
 	rc=$$?; \
 	if [ $$rc -eq 0 ]; then \
 		$(call DECOR_GREEN_LINE); \
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <cstring>
 
 #include "game.hpp"
 #include "constants.h"
@@ -8,8 +9,17 @@
 int main(void)
 {
 	srand(time(nullptr));
+
+	const char *qemu_headless = getenv("QEMU_HEADLESS");
+	if (qemu_headless && strcmp(qemu_headless, "1") == 0)
+	{
+		SetTraceLogLevel(LOG_INFO);
+		TraceLog(LOG_INFO,"QEMU Headless");
+		return 0; // No Graphics
+	}
+
 	Game game;
 	game.Run();
 
-	return 1; //success
+	return 1; // success
 }
